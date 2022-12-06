482521b1a0576e40cb00919b2da39a118d202392
unknown <bleachevil@gmail.com>
2022-12-05 20:38:24 -0500

    Updated

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -1,5 +1,5 @@
 const router = require('express').Router();
-const { User } = require('../../models');
+const { User, Books } = require('../../models');
 
 // CREATE new user
 router.post('/', async (req, res) => {
@@ -72,6 +72,30 @@ router.post('/logout', (req, res) => {
     });
   } else {
     res.status(404).end();
+  }
+});
+
+// Update fav
+
+router.put('/books/:id', async (req, res) => {
+  // Where is this action method sending the data from the body of the fetch request? Why?
+  // It is sending the data to the Model so that one dish can be updated with new data in the database.
+  try {
+    const book = await Books.update(
+      {
+        user_id: req.body.user_id,
+      },
+      {
+        where: {
+          id: req.params.id,
+        },
+      }
+    );
+    // If the database is updated successfully, what happens to the updated data below?
+    // The updated data (dish) is then sent back to handler that dispatched the fetch request.
+    res.status(200).json(book);
+  } catch (err) {
+    res.status(500).json(err);
   }
 });
 
---
+++ public/js/add-fav.js
@@ -0,0 +1,33 @@
+async function editFormHandler(event) {
+    event.preventDefault();
+    const user_id = document.querySelector('#quantity').value;
+  
+    // window.location gives us access to the URL. We then use the .split() method to access the number at the end of the URL and set that equal to id.
+    const id = window.location.toString().split('/')[
+      window.location.toString().split('/').length - 1
+    ];
+  
+    // What part of our application will handle this 'put' request?
+    // The Controller will handle this 'put' request.
+  
+    const response = await fetch(`/api/books/${id}`, {
+      method: 'PUT',
+      body: JSON.stringify({
+        user_id,
+      }),
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    });
+  
+    // What happens if the response is ok?
+    // If the response is ok, that means that the dish was updated successfully. 
+    if (response.ok) {
+      document.location.replace(`/books/${id}`);
+    } else {
+      alert('Failed to edit book');
+    }
+  }
+  
+  document.querySelector('.edit-book-form').addEventListener('submit', editFormHandler);
+  
